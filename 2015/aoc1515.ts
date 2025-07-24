66a1bc722776b69b913495ed8c3e7fb31336e6d4
Chad Walker <cwalker@mozilla.com>
2025-07-24 15:01:36 -0500

    random code sitting in the repo

--- 2015/aoc1515.ts
+++ 2015/aoc1515.ts
@@ -1,28 +1,46 @@
-import { NotImplemented, Options, run } from "aoc-copilot";
+import { argsToOptions NotImplemented, Options, run } from "aoc-copilot";
 
 type AdditionalInfo = {
     [key: string]: string;
 };
 
+type Ingredient = {
+    name: string;
+    capacity: number;
+    durability: number;
+    flavor: number;
+    texture: number;
+    calories: number;
+};
+
+const RE = /(?<name>\w+): capacity (?<capacity>-?\d+), durability (?<durability>-?\d+), flavor (?<flavor>-?\d+), texture (?<texture>-?\d+), calories (?<calories>-?\d+)/u;
 export async function solve(
     input: string[],
     part: number,
     test: boolean,
     additionalInfo?: AdditionalInfo,
 ): Promise<string | number> {
+    const ingrendients: Ingredient[] = [];
+    for (const line of input) {
+        const match = line.match(RE);
+        if (!match || !match.groups) {
+            throw new Error('RE');
+        }
+        ingrendients.push({
+            name: match.groups.name,
+            capacity: parseInt(match.groups.capacity, 10),
+            durability: parseInt(match.groups.durability, 10),
+            flavor: parseInt(match.groups.flavor, 10),
+            texture: parseInt(match.groups.texture, 10),
+            calories: parseInt(match.groups.calories, 10),
+        });
+    }
     if (part === 1) {
-        throw new NotImplemented('Not Implemented');
+        
     }
     throw new NotImplemented('Not Implemented');
 }
 
-const options: Options = {};
-const args: string[] = process.argv.splice(2);
-for (const arg of args) {
-    if (arg === '-t') options.testsOnly = true;
-    if (arg === '-s') options.skipTests = true;
-    if (arg === '1') options.onlyPart = 1;
-    if (arg === '2') options.onlyPart = 2;
-}
+const options = argsToOptions(process.argv);
 
 run(__filename, solve, options);
