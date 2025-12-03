12852d9f2cb528b7897ea7bbe8ac3b66b759fde2
Chad Walker <cwalker@mozilla.com>
2025-12-02 23:20:59 -0600

    2025, day 3

--- 2025/aoc2503.ts
+++ 2025/aoc2503.ts
@@ -4,16 +4,40 @@ type AdditionalInfo = {
     [key: string]: string;
 };
 
+function maxJoltage(bank: number[], digits = 2, carryIn = 0): number {
+    if (digits < 1) {
+        throw new Error('invalid number of digits');
+    }
+    let max = bank[0];
+    let pos = 0;
+    for (let i = 1; i <= bank.length - digits; i++) {
+        if (bank[i] > max) {
+            max = bank[i];
+            pos = i;
+        }
+    }
+    if (digits === 1)
+        return carryIn * 10 + max;
+    return maxJoltage(bank.slice(pos + 1), digits - 1, carryIn * 10 + max);
+}
+
 export async function solve(
     input: string[],
     part: number,
     test: boolean,
     additionalInfo?: AdditionalInfo,
 ): Promise<string | number> {
-    if (part === 1) {
-        throw new NotImplemented('Not Implemented');
+    const banks: number[][] = [];
+    for (const line of input) {
+        if (line.trim().length === 0) continue;
+        banks.push(line.split('').map(Number));
     }
-    throw new NotImplemented('Not Implemented');
+    let total = 0;
+    const digits = part === 1 ? 2 : 12;
+    for (const bank of banks) {
+        total += maxJoltage(bank, digits);
+    }
+    return total;
 }
 
 run(__filename, solve);
